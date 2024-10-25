b053a98673299bbc69d0c70e8a29db509a4dcd68
rubyci-setup <rubyci.setup@gmail.com>
2024-10-25 12:37:14 +0200

    Update public_controller_test.rb

--- test/controllers/public_controller_test.rb
+++ test/controllers/public_controller_test.rb
@@ -6,295 +6,171 @@ class PublicControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get index" do
+  test "should not get index" do
+    get root_url
+    assert_response :not_found
+  end
+
+  test "should 1 index" do
     get root_url
     assert_response :success
   end
 
-  test "should get index" do
+  test "should 2 index" do
     get root_url
     assert_response :success
   end
 
-  test "should get index" do
+  test "should 3 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 4 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 5 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 6 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 7 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 8 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 9 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 10 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 11 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 12 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 13 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 14 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
-    get root_url
-    assert_response :success
-  end
-  test "should get index" do
+  test "should 15 index" do
     get root_url
     assert_response :success
   end
 
-  test "should get index" do
+  test "should 16 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 17 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 18 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 19 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 20 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 21 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 22 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 23 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 24 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 25 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 26 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 27 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 28 index" do
     get root_url
     assert_response :success
   end
-  test "should get index" do
+  test "should 29 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 30 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 31 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 32 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 33 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 34 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 35 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 36 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 37 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 38 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 39 index" do
+    get root_url
+    assert_response :success
+  end
+  test "should 40 index" do
     get root_url
     assert_response :success
   end
